eece803c5dcbf2762f568e74f7755364f83a3c32
root <root@DESKTOP-6I3RBRC>
2023-06-12 17:09:02 +0300

    First Commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code in this file!
+let currentUser = 'my words';
+let welcomeMessage = "Welcome to Flatbook, ";
+currentUser = currentUser.toUpperCase();
+welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}`;
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+let shortGreeting = "Welcome, ";
+shortGreeting = `Welcome, ${currentUser}!`;
+const firstInitial = currentUser.slice(0, 1);
+shortGreeting = `Welcome, ${firstInitial}!`;
+
+
+
